72f6a48f79b4f96543834e39b0cc67630876e92d
priti bishnoi <108807403+priti9770@users.noreply.github.com>
2023-01-05 15:34:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import NotificationToast from "./tostifyNotification/Mytostify";
+import Todo from "./components/Todo.jsx";
+function App() {
+  return (
+    <>
+      {/* <NotificationToast /> */}
+      <Todo />
+    </>
+  );
+}
+
+export default App;
